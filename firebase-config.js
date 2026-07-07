55bd9b09e09e0707686392dc62f192d4e6fa2d78
Fk-edits <fiqkirhbw@gmail.com>
2026-07-07 18:36:08 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,18 @@
-// ===== firebase-config.js =====
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-import { FIREBASE_CONFIG } from './config.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-const app = initializeApp(FIREBASE_CONFIG);
+// Hardcode keys directly (like your old site)
+const firebaseConfig = {
+  apiKey: "AIzaSyBVWaHveZgGgcAcgojBMDmDdu1fdeJBgU4",
+  authDomain: "danfer-8bfd4.firebaseapp.com",
+  projectId: "danfer-8bfd4",
+  storageBucket: "danfer-8bfd4.firebasestorage.app",
+  messagingSenderId: "825811670471",
+  appId: "1:825811670471:web:48bf9fa767bedee21075e2",
+  measurementId: "G-Y888P5969S"
+};
+
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
